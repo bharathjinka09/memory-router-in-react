7bd4b7c9089a53d4e11f8a5fcd6296f99dae5e22
bharath <bharath.ranga@medleymed.com>
2021-04-02 12:42:39 +0530

    Updates

---
+++ precache-manifest.0f49ba2031f59e0a0add6bec00211d10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f6e16ae607460d25689",
+    "url": "/memory-router-in-react/static/css/main.c721a2f5.chunk.css"
+  },
+  {
+    "revision": "8f6e16ae607460d25689",
+    "url": "/memory-router-in-react/static/js/main.c6964548.chunk.js"
+  },
+  {
+    "revision": "ca9c5fef2b6d8e70025a",
+    "url": "/memory-router-in-react/static/js/runtime~main.2867a969.js"
+  },
+  {
+    "revision": "ebe1bcac775f9505cbe8",
+    "url": "/memory-router-in-react/static/js/2.f14fced9.chunk.js"
+  },
+  {
+    "revision": "05a3676800c9f47ae0cd252464579cba",
+    "url": "/memory-router-in-react/index.html"
+  }
+];
